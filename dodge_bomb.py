2e84e24c27588b8d7085df9677de0abf4a5ad7ad
c0c2400842 <c0c2400842@edu.teu.ac.jp>
2025-04-22 14:48:48 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,8 @@
 import os
+import random
 import sys
 import pygame as pg
+
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -13,14 +15,39 @@ DELTA = { #カンマ忘れんな
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+
+    """
+    引数:こうかとんRectまたは爆弾Rect
+    戻り値:判定結果タプル(ヨコ、タテ)
+    画面内ならTrue 画面外ならFalse
+    """
+
+    yoko,tate = True, True  #ヨコタテ用の変数
+    #横方向判定 
+    if rct.left < 0 or WIDTH <rct.right:  #もしも画面外だったら
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
+    #こうかとん初期化
     bg_img = pg.image.load("fig/pg_bg.jpg")    
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    #爆弾初期化
+    bb_img = pg.Surface((20, 20))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
+    bb_img.set_colorkey((0, 0, 0))
+    bb_rct = bb_img.get_rect()
+    bb_rct.center = random.randint(0,WIDTH),random.randint(0,HEIGHT)
+    vx, vy = +5, +5
+
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -38,7 +65,19 @@ def main():
             
        
         kk_rct.move_ip(sum_mv)
-        screen.blit(kk_img, kk_rct)
+        if check_bound(kk_rct) != (True, True): #画面外だったら
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])#画面内に戻す
+
+
+        screen.blit(kk_img, kk_rct) #こうかとんを描画
+        bb_rct.move_ip(vx,vy)#爆弾の移動
+        yoko,tate = check_bound(bb_rct)
+        if not yoko:#左右どちらかにはみ出ていたら
+            vx *= -1
+        if not tate:
+            vy *= -1
+
+        screen.blit(bb_img, bb_rct) #爆弾を描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
